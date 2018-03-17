61c57b94f74956f4120338bd76f6b654a44bb4ae
Unknown <ahmedz.tarek92@gmail.com>
2018-03-17 03:57:53 +0200

    init new project

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+import models
+
+member1=models.Member("ahmad","25")
+member2=models.Member("ali","23")
+
+post1=models.Post("Title1","Content1")
+post2=models.Post("Title2","Content2")
+post3=models.Post("Title3","Content3")
